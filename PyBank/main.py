783ae4fd0651fd31f3caf5b90b5c2626c4a65721
AshleyJubilee <ashleybjubilee@gmail.com>
2023-11-15 19:46:08 -0700

    PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,40 +1,69 @@
 import os
 import csv
+import statistics
 
 csvpath = os.path.join("Resources", "budget_data.csv")
 
 monthTotal = 0
 profitTotal  = 0
 profitChange = 0
-changeList  = []
-firstMonth = 0
+changeList = []
+currentMonth = 0
 lastMonth = 0
+averageChange = 0
+greatestInc = 0
+greatestIncMonth = ''
+greatestDec = 0
+greatestDecMonth = ''
+diff = 0
 
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter= ',')
 
-    #Stores and skips header
+    # Stores and skips header
     csvHeader = next(csvreader)
 
-    # Counts number of rows after header, number of months
+    # Loops through each row and updates vairables
     for row in csvreader:
+
+        # Counts total monthts and total profits
         monthTotal += 1
         profitTotal += int(row[1])
 
-        if firstMonth == 0:
-            firstMonth = int(row[1])
-            continue
-        else: 
+        currentMonth = int(row[1])
+      
+        # Skips first month since there is no 'last month' to compare the current value to
+        if lastMonth == 0: 
             lastMonth = int(row[1])
+            continue
+        else:
 
-    totalChange = lastMonth - firstMonth
-    averageChange = totalChange / monthTotal
+            # Updates current difference and adds it to the list
+            diff = -(lastMonth - currentMonth)
 
+            changeList.append(diff)
+
+            # Finds the mean of all month to month differences
+            averageChange = round(statistics.mean(changeList),2)
+
+            # Updates greatest change variables
+            if diff > greatestInc:
+                greatestInc = diff
+                greatestIncMonth = row[0]
+        
+            if diff < greatestDec:
+                greatestDec = diff
+                greatestDecMonth = row[0]
+
+            # Sets the lastMonth value for the next loop
+            lastMonth = int(row[1])
 
     print(f"Total Months: {monthTotal}")
     print(f"Total Profit: ${profitTotal}")
     print(f"Average Change: ${averageChange}")
+    print(f"Greatest Increase in Profits: {greatestIncMonth} (${greatestInc})")
+    print(f"Greatest Decrease in Profits: {greatestDecMonth} (${greatestDec})")
           
 
 
